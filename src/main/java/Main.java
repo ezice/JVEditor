446d90b7fbe85c1ada75cc71f2709c36b3780d41
ezice <rhuff2@yahoo.com>
2015-11-19 07:33:24 -0600

    Add description to list.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,10 +23,13 @@ public class Main {
 	public static void main(String[] args) {
 		MidiDevice.Info midiInfo[] = MidiSystem.getMidiDeviceInfo();
 		JFrame frame = new JFrame("JVEditor");
+		/*
+		 * Create an Edit/Preferences menu where a user can select the desired MIDI device.
+		 */
 		JTextArea textArea = new JTextArea(getMidiDevices(midiInfo));
 		frame.add(textArea);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(300, 300);
+		frame.setSize(600, 300);
 		frame.setVisible(true);
 	}
 	
@@ -34,7 +37,7 @@ public class Main {
 		StringBuilder midiDevices = new StringBuilder();
 		
 		for(MidiDevice.Info info : midiInfo) {
-			midiDevices.append(info.toString() + "\n");
+			midiDevices.append(info.getName() + ":" + info.getDescription() + "\n");
 			LOG.debug(info);		
 		}
 		
