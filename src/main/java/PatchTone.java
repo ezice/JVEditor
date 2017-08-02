dc4792c1c4aacd659ad79aac651eb5e6dad8b045
Rick Huff <rhuff@austin.utexas.edu>
2017-08-02 07:15:51 -0500

    Daily commit.

--- src/main/java/PatchTone.java
+++ src/main/java/PatchTone.java
@@ -40,8 +40,20 @@ public class PatchTone {
 	}
 	
 	public enum RandomPitchDepth {
-		D0, D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D20, D30, D40, D50, D60, D70, D80, D90, D100, D200, D300, D400, D500, 
-		D600, D700, D800, D900, D1000, D1100, D1200
+		RPD0, RPD1, RPD2, RPD3, RPD4, RPD5, RPD6, RPD7, RPD8, RPD9, RPD10, RPD20, RPD30, RPD40, RPD50, RPD60, RPD70, RPD80, RPD90, RPD100, RPD200, RPD300, RPD400, RPD500, 
+		RPD600, RPD700, RPD800, RPD900, RPD1000, RPD1100, RPD1200
+	}
+	
+	public enum PitchKeyFollow {
+		PKF_100, PKF_70, PKF_50, PKF_30, PKF_10, PKF0, PKF10, PKF20, PKF30, PKF40, PKF50, PKF70, PKF100, PKF120, PKF150, PKF200
+	}
+	
+	public enum OutputAssign {
+		MIX, EFX, DIRECT1, DIRECT2
+	}
+	
+	public enum EnvelopeTime {
+		ET_100, ET_70, ET_50, ET_40, ET_30, ET_20, ET_10, ET0, ET10, ET20, ET30, ET40, ET50, ET70, ET100
 	}
 	
 	boolean toneSwitch;
@@ -62,4 +74,12 @@ public class PatchTone {
 	LFO[] lfo = new LFO[2];
 	int coarseTune, fineTune;
 	RandomPitchDepth randomPitchDepth;
+	PitchKeyFollow pitchKeyFollow;
+	int pitchEnvelopeDepth, pitchEnvelopeVelocitySensitivity, pitchEnvelopeTimeKeyFollow;
+	int pitchEnvelopeTime = new int[4];
+	int pitchEnvelopeLevel = new int[4];
+	int pitchLFODepth = new int[2];
+	EnvelopeTime pitchEnvelopeVelocityTime1, pitchEnvelopeVelocityTime4;
+	int randomPanDepth, alternatePanDepth, panLFO1Depth, panLFO2Depth;
+	OutputAssign outputAssign;
 }
